e412d33567ac290998fa0cc96e996d9c3dc5e30b
Simon Deeb <simon19-meet@meet.mit.edu>
2018-08-08 01:56:03 -0700

    finished lab without extras

--- exercises/app.py
+++ exercises/app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/')
 def home_page():
-    player_list=["Messi","Neymar","Ter Stegen","Cavani","Griezman","Pogba"]
-    for i in player_list:
-        print(i)
-    return render_template("index.html")
+    likes_same_sport=False
+    arr=["Messi","Neymar","Ter Stegen","Cavani","Griezman","Pogba"]
+    if likes_same_sport:
+        return render_template("index.html", player_list=arr,likes_same_sport=likes_same_sport)
+    return render_template("index.html",likes_same_sport=likes_same_sport)
 
 if __name__ == '__main__':
    app.run(debug = True)
